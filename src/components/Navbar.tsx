e2a46d6d34dba8138aeac496c3a1fa9154831de3
Yann Klein <yann.klein@me.com>
2025-01-31 13:58:28 +0900

    add bootstrap and style navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,25 @@
-import React from 'react';
-
 const Navbar = () => {
   return (
-    <div className="navbar navbar-expand-sm navbar-light navbar-lewagon">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/logo.png" />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="https://www.lewagon.com/tokyo">
-                LW Tokyo
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="https://tokyo-events.herokuapp.com/">
-                Tokyo Tech Event API
-              </a>
-            </li>
-          </ul>
-        </div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary py-3">
+      <div className="container-fluid d-flex justify-content-between">
+        <img
+          height="64"
+          src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/logo.png"
+        />
+        <ul className="navbar-nav mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" href="https://www.lewagon.com/tokyo">
+              LW Tokyo
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="https://yannklein.dev/">
+              About Yann
+            </a>
+          </li>
+        </ul>
       </div>
-    </div>
+    </nav>
   );
 };
 
