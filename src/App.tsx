176c3841810362f4c68647fa89ba32b903993e97
Yann Klein <yann.klein@me.com>
2025-02-05 16:24:25 +0900

    remove useless routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import EventsIndex from './components/EventsIndex';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter basename='/tech-eventboard'>
       <div className="min-vh-100 vh-100 d-flex flex-column">
-        <Routes>
-          <Route path="/" element={<EventsIndex />} />
-        </Routes>
+      <EventsIndex />
       </div>
     </BrowserRouter>
   );
