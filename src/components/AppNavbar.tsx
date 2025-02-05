e2ade3d1c39cf107417522c741eedebbda1a8203
Yann Klein <yann.klein@me.com>
2025-02-05 22:27:38 +0900

    refactor navbar code

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -5,22 +5,24 @@ import { capitalize } from '../utils';
 import tokyoTower from '../assets/tokyo-tower.svg';
 import sagradaFamilia from '../assets/sagrada-familia.png';
 
+const cityEmoji: Record<string, string> = {
+  tokyo: tokyoTower,
+  barcelona: sagradaFamilia
+}
+
 const AppNavbar = ({city}: {city: string}) => {
-  const cityEmoji: Record<string, string> = {
-    tokyo: tokyoTower,
-    barcelona: sagradaFamilia
-  }
+  const capitalizedCity = capitalize(city);
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid className="d-flex ">
         <Navbar.Brand href="#" className="d-flex align-items-center gap-2 gap-lg-3">
           <img
             height="64"
-            src={cityEmoji[city]}
-            alt="logo"
+            src={cityEmoji[city] || tokyoTower}
+            alt={`${capitalizedCity} logo`}
           />
           <h1 className="mb-0">
-            {capitalize(city)} tech events
+            {capitalizedCity} tech events
             <br className="d-block d-lg-none" /> calendar
           </h1>
         </Navbar.Brand>
@@ -28,7 +30,7 @@ const AppNavbar = ({city}: {city: string}) => {
         <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
           <Nav>
             <Nav.Link href={`https://www.lewagon.com/${city}`} target="_blank">
-              LW {capitalize(city)}
+              LW {capitalizedCity}
             </Nav.Link>
             <Nav.Link href="https://yannklein.dev/" target="_blank">
               About Yann
