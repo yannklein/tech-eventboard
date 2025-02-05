f140d76a723420a7ed35aef2f90f253453815095
Yann Klein <yann.klein@me.com>
2025-02-05 22:33:01 +0900

    modify navbar links

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -4,18 +4,23 @@ import Navbar from 'react-bootstrap/Navbar';
 import { capitalize } from '../utils';
 import tokyoTower from '../assets/tokyo-tower.svg';
 import sagradaFamilia from '../assets/sagrada-familia.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const cityEmoji: Record<string, string> = {
   tokyo: tokyoTower,
-  barcelona: sagradaFamilia
-}
+  barcelona: sagradaFamilia,
+};
 
-const AppNavbar = ({city}: {city: string}) => {
+const AppNavbar = ({ city }: { city: string }) => {
   const capitalizedCity = capitalize(city);
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid className="d-flex ">
-        <Navbar.Brand href="#" className="d-flex align-items-center gap-2 gap-lg-3">
+        <Navbar.Brand
+          href="#"
+          className="d-flex align-items-center gap-2 gap-lg-3"
+        >
           <img
             height="64"
             src={cityEmoji[city] || tokyoTower}
@@ -29,11 +34,21 @@ const AppNavbar = ({city}: {city: string}) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
           <Nav>
-            <Nav.Link href={`https://www.lewagon.com/${city}`} target="_blank">
-              LW {capitalizedCity}
+            <Nav.Link
+              href="https://www.linkedin.com/in/yann-klein/"
+              target="_blank"
+            >
+              <FontAwesomeIcon className="fs-4" icon={faLinkedin} />
             </Nav.Link>
-            <Nav.Link href="https://yannklein.dev/" target="_blank">
-              About Yann
+            <Nav.Link href="https://github.com/yannklein" target="_blank">
+              <FontAwesomeIcon className="fs-4" icon={faGithub} />
+            </Nav.Link>
+            <Nav.Link
+              href="https://yannklein.dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Portfolio
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
