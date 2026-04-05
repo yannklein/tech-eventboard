3c64ff90a20ded5e497272706c4e89e1332facc6
Yann Klein <yann.klein@me.com>
2026-04-05 21:16:32 +0200

    refactor: update 404 page layout and styling for improved user experience

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename='/tech-eventboard'>
+    <BrowserRouter basename='/'>
       <div className="min-vh-100 vh-100 d-flex flex-column">
       <EventsIndex />
       </div>
